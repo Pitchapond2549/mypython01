0f68e57c037dd6eee0d648ed0add208a4e949e5f
lSoLoVEl <s6519410044@sau.ac.th>
2025-08-22 10:26:38 +0700

    06-08

---
+++ teas06.py
@@ -0,0 +1,26 @@
+#operator ตัวดำเนินการ ที่ใช้ในการ ประมวลผลข้อมูล
+#1.Arithmetic operator เครื่องหมายคำนวณ 
+# (+,-,*,/,%mod,// floor,** eponetial)
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 % 3)  #หารเอาเศษ เลขจำนวนเต็มเท่านั้น
+print(10 //3)  #หารเอาส่วนเ ลขจำนวนเต็มเท่านั้น
+print(10 **3) 
+print("---------")
+# comparison operator เครื่องหมายเปรียบเทียบ
+print(10 == 3)
+print(10 != 3)
+print('bangna'>= ' bangpoo')
+print('thai' < 'THAI')
+print('-----------')
+# logical operator เครื่องหมายตรรกะ not , and ,or 
+# ข้อมูลที่จะใช้ต้องเป็น True / False ผลลัพธ์ได้แค่  True / False
+print(not True)
+print(not False)
+print(True and True) #True
+print(True and False)
+print(False and True)
+print(False or False) #False
+print(False or True)
